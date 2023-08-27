c5577e8158373edc68f9d7d71ea1de93f3efb377
aoton0029 <aotonnnnn@gmail.com>
2023-08-27 16:11:25 +0900

    upd

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template,request
-#下記のインポート文を追加
-from models.models import OnegaiContent
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,21 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    name = request.args.get("name")
-    #以下を変更
-    all_onegai = OnegaiContent.query.all()
-    return render_template("index.html",name=name,all_onegai=all_onegai)
-    #変更終わり
+    return render_template("index.html")
 
 
 @app.route("/index",methods=["post"])
 def post():
-    name = request.form["name"]
-    #ここも変更
-    all_onegai = OnegaiContent.query.all()
-    return render_template("index.html", name=name, all_onegai=all_onegai)
-    #変更終わり
+    return render_template("index.html")
 
+@app.route("/grid")
+def gridview():
+    return render_template("grid.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/static/js/grid.js
+++ app/static/js/grid.js
@@ -1,61 +1,81 @@
-let data = [
-    {"工番":"AAAAA", "型名": "SAMPLE_1", "着手計画日":new Date(2023/8/23)},
-    {"工番":"BBBBB", "型名": "SAMPLE_2", "着手計画日":new Date(2023/8/23)},
-    {"工番":"CCCCC", "型名": "SAMPLE_3", "着手計画日":new Date(2023/8/24)},
-    {"工番":"DDDDD", "型名": "SAMPLE_4", "着手計画日":new Date(2023/8/25)},
-    {"工番":"EEEEE", "型名": "SAMPLE_5", "着手計画日":new Date(2023/8/27)},
+let data = {
+    "工番":["AAAAA","BBBBB","CCCCC","DDDDD", "EEEEE"],
+    "型名":["SAMPLE_A","SAMPLE_B","SAMPLE_C","SAMPLE_D", "SAMPLE_E"],
+    "着手計画日":[new Date(2023/8/23),new Date(2023/8/23),new Date(2023/8/23),new Date(2023/8/23), new Date(2023/8/23)],
+}
+let new_data = [
+    {"工番":"AAAAA", "型名": "SAMPLE_1", "着手計画日":"2023/8/23"},
+    {"工番":"BBBBB", "型名": "SAMPLE_2", "着手計画日":"2023/8/23"},
+    {"工番":"CCCCC", "型名": "SAMPLE_3", "着手計画日":"2023/8/24"},
+    {"工番":"DDDDD", "型名": "SAMPLE_4", "着手計画日":"2023/8/25"},
+    {"工番":"EEEEE", "型名": "SAMPLE_5", "着手計画日":"2023/8/27"},
 ]
-var weeks = ["月","火","水","木","金"]
+const weeks = ["月","火","水","木","金"]
 window.onload = OnLoad()
 
 function OnLoad(){
-    //document.writeln('a');
     var grid = document.getElementById('grid');
-    var today = new Date();
-    var startDayOfWeek = new Date(today.year, today.month - 1, 1).getDay();
-    var countDay = 0;
-    var monthOfEndDay = new Date(today.year, today.month, 0).getDate();
-    document.writeln(startDayOfWeek);
-    for(var i =0;i < 100;i++){
-        document.writeln(i);
-        var tr_day = document.createElement('tr');
+    var _date = new Date(new_data[0]["着手計画日"]);
+    _date.setDate(_date.getDate() - _date.getDay());
+
+    function toString(date, day){
+        return (date.getMonth()+1) + '/' + date.getDate() + '(' + weeks[day] +')';
+    }
+    for(var i =0;i < 10;i++){
+        var main_tr = document.createElement('tr');
+        var sub_tr = document.createElement('tr');
         for(var day = 0;day<weeks.length;day++){
-            document.writeln(day);
-            var td_day = document.createElement('td');
-            var today_day = startDayOfWeek.setDate(startDayOfWeek.getDate() + day);
-            document.writeln(today_day);
-            td_day.textContent = today_day.month + '/' + today_day.day + '(' + weeks[day] +')';
-            tr_day.appendChild(td_day);
+            // 日付行
+            let th = document.createElement('th');
+            th.textContent = toString(_date, day);
+            main_tr.appendChild(th);
+
+            // データ行
+            let td = document.createElement('td');
+            td.appendChild(createSubTable(_date));
+            sub_tr.appendChild(td);
+
+            _date.setDate(_date.getDate() + 1);
         }
-        grid.appendChild(tr_day);
-        // var tr_subtable = document.createElement('tr');
-        // for(var day = 0;day<weeks.length;day++){
-        //     // １行目で開始曜日と同じ場合
-        //     if (i == 0 && j == startDayOfWeek) {
-        //         // 日付+1
-        //         countDay++;
-        //         var td = document.createElement('td');
-        //         td.className = 'with_date';
-        //         td.innerHTML = countDay;    
-        //     }
-        //     else if (countDay != 0 && countDay < monthOfEndDay) {
-        //         // 日付が0以外で、日付が末日より小さい場合
-        //         // 日付+1
-        //         countDay++;
-        //         // tdタグ（日付有りが分かるようにクラス属性に"with_date"を設定）
-        //         _html += '<td class="with_date">' + countDay + '</td>';
-        //     }
-        //     else {
-        //         // tdタグ（日付無しが分かるようにクラス属性に"no_date"を設定）
-        //         _html += '<td class="no_date"></td>';
-        //     }
-        // }
+        grid.appendChild(main_tr);
+        grid.appendChild(sub_tr);
+        _date.setDate(_date.getDate() + 2);
     }
 
-    
+    reflect();
 }
 
-function createSubTable(){
-    var subtable = document.createElement('table');
-
+function reflect(){
+    for(var i =0;i<new_data.length;i++){
+        let d = new Date(new_data[i]["着手計画日"])
+        let grid = document.getElementById(toStringDate(d));
+        let tr = document.createElement('tr');
+        let keys = Object.keys(new_data[0])
+        for(var j=0;j<keys.length;j++){
+            let td = document.createElement('td');
+            td.textContent = new_data[i][keys[j]];
+            tr.appendChild(td);    
+        }
+        grid.appendChild(tr);
+    }
 }
+
+function createSubTable(date){
+    let subtable = document.createElement('table');
+    subtable.id = toStringDate(date);
+    subtable.className = "sub_grid";
+    let tr = document.createElement('tr');
+    var cols = Object.keys(new_data[0]);
+    for(var i =0;i<cols.length;i++){
+        let th = document.createElement('th');
+        th.textContent = cols[i];
+        tr.appendChild(th);
+    }
+    subtable.appendChild(tr);
+    return subtable;
+}
+
+function toStringDate(date){
+    return date.getFullYear() + "_" + (date.getMonth()+1) + "_" + date.getDate();
+}
+
